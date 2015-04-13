041b9befb3e002b61311ac210899926ebfb48ade
Anthony E. Alvarez <acunet3278@yahoo.com>
2015-04-12 20:45:26 -0400

    Web friendly printing
    Added css media query for printing. Added table that is smaller than
    containing table to make sure border printed.

--- gains.php
+++ gains.php
@@ -1,65 +1,85 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Dollars For Peace&trade;</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 		<meta http-equiv="keywords" content="Dollars For Peace, World Peace Society">
 		<link href="./css/styles.css" rel="stylesheet" type="text/css">
+		<style>
+			@media print {
+				#logo, #footer-nav {
+					display: none;
+				}
+				@page {
+					margin: 0.5cm;
+					display: inline ;
+				}
+			}
+		</style>
 	</head>
 	<body link="#666666" alink="#660000" vlink="#333333">
-		<table width="720" border="0" cellspacing="0" align="center" cellpadding="0">
+		<table width="720px" border="0" cellspacing="0" align="center" cellpadding="0">
 			<tr>
-				<td width="720" valign="top" align="left">
+				<td valign="top" align="left">
 				<center>
+
 					<a href="index.php"> <font size="6"><b>DOLLARS FOR PEACE&trade;</b></font>
-					<br>
-					<br>
-					<img src="http://worldpeacesociety.org/images/worldpeacesociety_logo2.jpg" title="Dollars for Peace&trade;" alt="Dollars for Peace&trade;" border="0"></a>
+					<div id="logo">
+						<br>
+						<br>
+						<img src="http://worldpeacesociety.org/images/worldpeacesociety_logo2.jpg" title="Dollars for Peace&trade;" alt="Dollars for Peace&trade;" border="0">
+					</a>
+					</div>
 					<br>
 					<br>
 					<font size="4"><b>GAINS FOR ALL</b></font>
 				</center></td>
 			</tr>
 			<tr>
-				<td><p>
-				<font size="3"> Many non-profits look for several ways to raise money. They solicit, advertise, offer membership, and stage special events. We have unique objectives we want to achieve. Basically we want to bring to the whole world a new awareness and concern about the need for universal peace and the ability to achieve it now. The world can no longer afford the cost and dangers that war and violence impose. Though many think war and violence will always be with us, the fact is these problems are created by man so human intervention can end them and <b>every human being should try to do something about it.</b>
-					<br>
-					<br>
-					Certainly it makes sense to offer our contributors, who care most about our objectives, an incentive and compensation, if they wish to solicit additional participants to our cause. It is true, if you are solicited and do not wish to have your solicitor compensated, they won&#39;t be. But it doesn&#39;t change the amount you give. It seems to us, you should respect and thank the solicitor for bringing this to your attention and urging you to give to such a worthy cause. <b>In this way, the whole world gains.</b>
-					<br>
-					<br>
-					In fact, we even offer a very attractive bonus to both participants and sponsors who spend serious efforts on this and earn over $1,000 a year. They will receive a bonus equivalent to 50% payment of what the one they directly enrolled earns, if the enrollee also earns over $1,000 for the year. This offers tremendous leverage and a substantial income to <b>those that want to most help us.</b>
-					<br>
-					<br>
-                        Some people mistakenly feel this bonus payment represents a multi-level or pyramid sales practice, but such impression is extremely far from the truth. The fact is that your revenue at all times is only derived from the persons you directly enroll and no one else. If the parties you enlist produce more and you produce more, you earn a bonus. Such principle is at the heart of most any enterprise. Those who produce more value are normally paid more. Certainly a car salesman who sells a high-priced automobile is normally paid more than if he sells a cheaper model. A real estate salesman who sells a mansion normally makes more than if he sells a shack, and the list goes on and on. We hope this explanation settles this question. </p>
-				</td>
+				<td>
+				<p>
+					<font size="3"> Many non-profits look for several ways to raise money. They solicit, advertise, offer membership, and stage special events. We have unique objectives we want to achieve. Basically we want to bring to the whole world a new awareness and concern about the need for universal peace and the ability to achieve it now. The world can no longer afford the cost and dangers that war and violence impose. Though many think war and violence will always be with us, the fact is these problems are created by man so human intervention can end them and <b>every human being should try to do something about it.</b>
+						<br>
+						<br>
+						Certainly it makes sense to offer our contributors, who care most about our objectives, an incentive and compensation, if they wish to solicit additional participants to our cause. It is true, if you are solicited and do not wish to have your solicitor compensated, they won&#39;t be. But it doesn&#39;t change the amount you give. It seems to us, you should respect and thank the solicitor for bringing this to your attention and urging you to give to such a worthy cause. <b>In this way, the whole world gains.</b>
+						<br>
+						<br>
+						In fact, we even offer a very attractive bonus to both participants and sponsors who spend serious efforts on this and earn over $1,000 a year. They will receive a bonus equivalent to 50% payment of what the one they directly enrolled earns, if the enrollee also earns over $1,000 for the year. This offers tremendous leverage and a substantial income to <b>those that want to most help us.</b>
+						<br>
+						<br>
+						Some people mistakenly feel this bonus payment represents a multi-level or pyramid sales practice, but such impression is extremely far from the truth. The fact is that your revenue at all times is only derived from the persons you directly enroll and no one else. If the parties you enlist produce more and you produce more, you earn a bonus. Such principle is at the heart of most any enterprise. Those who produce more value are normally paid more. Certainly a car salesman who sells a high-priced automobile is normally paid more than if he sells a cheaper model. A real estate salesman who sells a mansion normally makes more than if he sells a shack, and the list goes on and on. We hope this explanation settles this question.
+				</p></td>
 			</tr>
-			<tr bgcolor ="white">
-				<td style="border: 1px solid black;"><font size="3"><b><center>
-				<span>
-					In essence, we bring to you a most effective and easy way to help wipe out
-					<br />
-					war, terrorism, and violence throughout the globe.
-					<br />
-					If anyone has a better way that fits in with our current world, let them come forth with it.
-					<br />
-					As Franklin Delano Roosevelt said late in his life,<br />
-				</span>
-                    <br />
-				<span style="text-decoration: underline">
-					~LET US MAKE WAR IMPOSSIBLE TO PURSUE~</span> 
-				<br />
-					AND
-					<br />
-					WITHOUT CHANGING BUSINESS ACTIVITIES OR ANY OTHER COMMITMENTS
-					<br />
-					THOSE WHO HELP US
-					<br />
-					CAN BENEFIT IN THEIR OWN LIVES AND MISSIONS TOO.
-		
-                    </center></b><font size="3"></td>
-			</tr>
-		
+			</table>
+			<table width="700px" border="0" cellspacing="0" align="center" cellpadding="0">
+			
 			<tr>
+				<td style="border: 1px solid black;"><font size="3"><b>
+					<center>
+						<span> In essence, we bring to you a most effective and easy way to help wipe out
+							<br />
+							war, terrorism, and violence throughout the globe.
+							<br />
+							If anyone has a better way that fits in with our current world, let them come forth with it.
+							<br />
+							As Franklin Delano Roosevelt said late in his life,
+							<br />
+						</span>
+						<br />
+						<span style="text-decoration: underline"> ~LET US MAKE WAR IMPOSSIBLE TO PURSUE~</span>
+						<br />
+						AND
+						<br />
+						WITHOUT CHANGING BUSINESS ACTIVITIES OR ANY OTHER COMMITMENTS
+						<br />
+						THOSE WHO HELP US
+						<br />
+						CAN BENEFIT IN THEIR OWN LIVES AND MISSIONS TOO.
+
+					</center></b><font size="3"></td>
+			</tr>
+
+			<tr id="footer-nav">
 				<td>
 				<br>
 				<br>
