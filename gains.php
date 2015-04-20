796e586cb7ec2a0df0c3ef1b1edc27566126cf53
Anthony E. Alvarez <acunet3278@yahoo.com>
2015-04-20 14:07:33 -0400

    GAINS FOR ALL PAGE SECOND PARAGRAPH Change
    ON GAINS FOR ALL PAGE SECOND PARAGRAPH HAS TO BE CHANGED BECAUSE OF THE
    QUESTION I RAISED LAST NITE.

--- gains.php
+++ gains.php
@@ -41,7 +41,10 @@
 					<font size="3"> Many non-profits look for several ways to raise money. They solicit, advertise, offer membership, and stage special events. We have unique objectives we want to achieve. Basically we want to bring to the whole world a new awareness and concern about the need for universal peace and the ability to achieve it now. The world can no longer afford the cost and dangers that war and violence impose. Though many think war and violence will always be with us, the fact is these problems are created by man so human intervention can end them and <b>every human being should try to do something about it.</b>
 						<br>
 						<br>
-						Certainly it makes sense to offer our contributors, who care most about our objectives, an incentive and compensation, if they wish to solicit additional participants to our cause. It is true, if you are solicited and do not wish to have your solicitor compensated, they won&#39;t be. But it doesn&#39;t change the amount you give. It seems to us, you should respect and thank the solicitor for bringing this to your attention and urging you to give to such a worthy cause. <b>In this way, the whole world gains.</b>
+						Certainly it makes sense to offer our contributors,  who care most about our objectives,  an incentive and 
+     compensation if they wish to solicit additional participants to our cause.  Whether or not you are solicited, the amount
+     you donate is your decision.  It should be what you can afford.  Seems to us, you should respect and thank the solicitor
+     for bringing this to your attention and urging you to give to such a worthy cause.  <b>In this way, the whole world gains.</b>
 						<br>
 						<br>
 						In fact, we even offer a very attractive bonus to both participants and sponsors who spend serious efforts on this and earn over $1,000 a year. They will receive a bonus equivalent to 50% payment of what the one they directly enrolled earns, if the enrollee also earns over $1,000 for the year. This offers tremendous leverage and a substantial income to <b>those that want to most help us.</b>
